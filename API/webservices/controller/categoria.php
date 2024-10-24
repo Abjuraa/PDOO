07dc0f9dd384480d71b40dbf7357021ebf5170d8
Abjuraa <107774277+Abjuraa@users.noreply.github.com>
2024-10-24 17:26:02 -0500

    CRUD implementado

--- API/webservices/controller/categoria.php
+++ API/webservices/controller/categoria.php
@@ -15,9 +15,23 @@ switch($_GET["op"]){
         echo json_encode($datos);
         break;
 
-        case "GetId":
-            $datos = $categoria->get_by_id($body);
-            echo json_encode($datos);
-            break;                                                                                     
+    case "GetId":
+        $datos = $categoria->get_by_id($body);                                                      
+        echo json_encode($datos);
+        break;       
+        
+    case "Insert":
+        $datos = $categoria->insert_categoria($body["cat_nom"], $body["cat_obs"]);
+        echo json_encode("insert correcto");
+        break;
+
+    case "Update":
+        $datos = $categoria -> update_categoria($body["cat_id"], $body["cat_nom"], $body["cat_obs"]);
+        echo json_encode("Update correcto");
+        break;
+    case "Delete":
+        $datos = $categoria -> delete_categoria($body["cat_id"]);
+        echo json_encode("Categoria eliminada correctamente");
+        break;
 } 
 ?>      
--- API/webservices/models/categoria.php
+++ API/webservices/models/categoria.php
@@ -15,11 +15,43 @@ class Categoria extends Conectar{
     public function get_by_id(){
         $conectar = parent::iniciar_conexion();
         parent::set_names();
-        $sql = "SELECT * FROM `categoria` WHERE est = 1 AND cat_id = 2";
+        $sql = "SELECT * FROM `categoria` WHERE est = 1 AND cat_id";
         $sql = $conectar -> prepare($sql);
         $sql -> execute();  
 
         return $resultado = $sql -> fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function insert_categoria($cat_nom, $cat_obs){
+        $conectar = parent::iniciar_conexion();
+        parent::set_names();
+        $sql = "INSERT INTO `categoria`(cat_nombre, cat_obs, est) VALUES (?,?,'1');";
+        $sql = $conectar -> prepare($sql);
+        $sql -> bindValue(1, $cat_nom);
+        $sql -> bindValue(2, $cat_obs);
+        $sql -> execute();  
+
+        return $resultado = $sql -> fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function update_categoria($cat_id,$cat_nom, $cat_obs){
+        $conectar = parent::iniciar_conexion();
+        parent::set_names();
+        $sql = "UPDATE `categoria` SET cat_nombre = ?, cat_obs = ?  WHERE cat_id = ?;";
+        $sql = $conectar -> prepare($sql);
+        $sql -> bindValue(1, $cat_nom);
+        $sql -> bindValue(2, $cat_obs);
+        $sql -> bindValue(3, $cat_id);
+        $sql -> execute();  
+    }
+
+    public function delete_categoria($cat_id){
+        $conectar = parent::iniciar_conexion();
+        parent::set_names();
+        $sql = "DELETE FROM `categoria` WHERE cat_id = ?";
+        $sql = $conectar -> prepare($sql);
+        $sql -> bindValue(1, $cat_id);
+        $sql -> execute();
     }
 }                
 ?>
